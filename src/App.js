4aeede9b7ed14a939456bc6040548269cd25e1d9
1roo <93hanaru@gmail.com>
2023-06-13 15:12:28 +0900

    scss 작성중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import TodoTemplate from './component/TodoTemplate';
 
 function App() {
--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { MdDone } from 'react-icons/md';
+import { MdDeleteOutline } from 'react-icons/md';
+import './scss/TodoItem.scss';
 
 const TodoItem = () => {
   return (
-    <li className='todo-list-item'>TodoItem</li>
+    <li className='todo-list-item'>
+      <div className='check-circle'>
+          <MdDone />
+      </div>
+      <span className='text'>할일</span>
+      <div className='remove'>
+          <MdDeleteOutline />
+      </div>
+    </li>
   )
 }
 
