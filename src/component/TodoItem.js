3764803bf316fb17d4b137c4cc30d75b000a2f98
1roo <93hanaru@gmail.com>
2023-06-14 17:10:57 +0900

    header 할일 개수

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -5,13 +5,16 @@ import './scss/TodoItem.scss';
 import cn from 'classnames';
 
 
-const TodoItem = ({ item, remove }) => {
+const TodoItem = ({ item, remove, check }) => {
 
   const {id, title, done} = item;
 
   return (
     <li className='todo-list-item'>
-      <div className={cn('check-circle', {active: done})}>
+      <div 
+        className={cn('check-circle', {active: done})}
+        onClick={()=> check(id)}
+      >
           {done && <MdDone />}  
       </div>
       <span className={cn('text', {finish: done})}>{title}</span>
